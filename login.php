903caf49802bf928bf00d1d1b87c3b0a04136049
AngelMorquecho <morquechoangel30@gmail.com>
2024-05-07 21:35:47 -0600

    commit

---
+++ login.php
@@ -0,0 +1,63 @@
+<?php session_start();
+
+
+$conn = new mysqli('localhost', 'root', '', 'bruteforce');
+if (isset($_POST['Enviar'])) {
+    $email = $_POST['email'];
+    $clave = $_POST['clave'];
+
+    $sql = "SELECT nombre,password FROM users WHERE nombre ='$email' and password ='$clave'";
+    $execute = mysqli_query($conn, $sql);
+    $resultado = mysqli_fetch_assoc($execute);
+
+    //Shift + Alt + F para identar el código
+    $email_valido = $resultado['nombre'] ?? '';
+    $clave_valido = $resultado['password'] ?? '';
+
+
+    if ($email === $email_valido && $clave === $clave_valido) {
+        header('location: index.php');
+    } else {
+        echo 'Credenciales incorrectas';
+    }
+}
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login y Registro con HTML5 y CSS3</title>
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+    <!-- Estilos CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Favicon -->
+ 
+</head>
+<body>
+    <h1>Login</h1>
+    <main>
+        <article>
+            <section>
+                <div id="alert"></div>
+                <form method="post">
+        <input type="text" name="email" id="email" placeholder="Ingresar Email" required>
+        <br><br>
+
+        <input type="password" name="clave" id="clave" placeholder="Ingresar Clave" required>
+        <br><br>
+
+        <input type="submit" value="Enviar" name="Enviar"id='Enviar'>
+    </form>
+            </section>
+        </article>
+    </main>
+    
+</body>
+</html>
